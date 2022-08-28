130b9c21b9c3f25a19c0944d8491e98a076636e9
Adrian Mitre <mitrehealthcare@gmail.com>
2022-08-27 18:45:48 -0700

    general structure in progress

---
+++ controllers/books_controller.js
@@ -0,0 +1,43 @@
+const router = require('express').Router()
+const db = require('../models')
+
+router.get('/', (req, res) => {
+  res.send('GET /places stub')
+})
+
+router.post('/', (req, res) => {
+  res.send('POST /places stub')
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  res.send('GET /places/:id stub')
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
+
+
+
---
+++ server.js
@@ -0,0 +1,30 @@
+//DEPENDENCIES
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+    )
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+
+
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
+  // LISTEN
+  app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+  })
